43fe270996273c57edc614d9f5c4d0c88ddaf0c6
Nicholas Hazel <nicholas.hazel@gmail.com>
2019-11-10 13:35:46 -0800

    Displaying possible lists and created a storage for our data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import {Component} from 'react';
 import List from './composition/List';
-import Card from './composition/Card';
-
 import './App.css';
 
-function App() {
-  return (
-    <section id="appWrapper">
-      <Card />
-      <List />
-    </section>
-  );
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      cards: {}      
+    }
+  }
+
+  render() {
+    const {store} = this.props;
+    console.log(store);
+    return (
+      <main id="appWrapper">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <section className="App-list">
+          <List />
+          {store.lists.forEach(id => {
+            console.log(id);
+          })}
+        </section>
+      </main>      
+    )
+  }
 }
 
 export default App;
