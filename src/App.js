af9fa5685ed6d625d9b76e3477bdc07e79f82bc5
Nicholas Hazel <nicholas.hazel@gmail.com>
2019-11-11 09:35:38 -0800

    Basic lists rendering.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
           <h1>Trelloyes!</h1>
         </header>
         <section className="App-list">
-          {store.lists.forEach(listItem => (
+          {store.lists.map(listItem => (
             <List
               key={listItem.id}
               header={listItem.header}
@@ -51,3 +51,25 @@ class App extends Component {
 }
 
 export default App;
+
+
+//   render() {
+//     const { store } = this.props
+//     return (
+//       <main className='App'>
+//         <header className='App-header'>
+//           <h1>Trelloyes!</h1>
+//         </header>
+//         <div className='App-list'>
+//           {store.lists.map(list => (
+//             <List
+//               key={list.id}
+//               header={list.header}
+//               cards={list.cardIds.map(id => store.allCards[id])}
+//             />
+//           ))}
+//         </div>
+//       </main>
+//     );
+//   }
+// }
